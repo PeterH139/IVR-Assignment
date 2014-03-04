f435cf6772c214135cbb53f74e3a31148bde26fa
Craig Wilkinson <git@craigwilkinson.co.uk>
2014-03-04 14:08:23 +0000

    Fixed handle error. Added a few useful comments.

--- drawMedianVideo.m
+++ drawMedianVideo.m
@@ -7,25 +7,35 @@ filenames = dir([file_dir '*.jpg']);
 
 median = getMedianFrame(file_dir,1);
 frame = imread([file_dir filenames(1).name]);
-figure(1); h1 = imshow(median);
+figure(2); h1 = imshow(frame);
 hold on;
 prev_pos = plot(0,0);
 hold off;
 
-% Read one frame at a time.
+
+% This is our main loop over each frame
 for k = 250 : size(filenames,1)
+    % read the frame
     current_frame = imread([file_dir filenames(k).name]);
+
+    % perform background subtraction
     frame = subtractMedian(median, current_frame);
+    
+    % make the frame black and white (this also performs a gaussian
+    % blur over the image beforehand to smooth)
     binary_frame = makeBinaryFrame(frame);
     
     %masked_frame = current_frame .* uint8(binary_frame);
     
+    % get the region information from the frame.
+    % this also performs the 'erode' function to eliminate
+    % small parts such as the cloth being hit.
     region_data = getRegionData(binary_frame);
        
     [n m] = size(region_data);
     centroid = [];
     if (n >= 1)
-        %delete(prev_pos);
+        delete(prev_pos);
         for i = 1 : n
             centroid_list = [region_data(i).Centroid;centroid];
         end
@@ -37,7 +47,8 @@ for k = 250 : size(filenames,1)
         hold off;
     end
     
-    set(h1, 'CData', frame);
+    set(h1, 'CData', current_frame);
+
     drawnow('expose');
     
     disp(['showing frame ' num2str(k)]);
--- getRegionData.m
+++ getRegionData.m
@@ -4,7 +4,7 @@ function [ out ] = getRegionData( frame )
 
     twod_binary_frame = squeeze(frame(:,:,1));
     
-    bwmorph(twod_binary_frame,'erode',1);
+    twod_binary_frame = bwmorph(twod_binary_frame,'erode',1);
 
     I = logical(twod_binary_frame);
     out = regionprops(I,'Centroid','ConvexHull','ConvexArea');
